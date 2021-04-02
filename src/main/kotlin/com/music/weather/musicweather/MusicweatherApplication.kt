9adb37cdd4c90a3508f522b9a283b479ad31a3a8
gspaiva <gabriel_silvapaiva@hotmail.com>
2021-04-02 18:32:57 -0300

    config swagger to the music route

--- src/main/kotlin/com/music/weather/musicweather/MusicweatherApplication.kt
+++ src/main/kotlin/com/music/weather/musicweather/MusicweatherApplication.kt
@@ -2,6 +2,8 @@ package com.music.weather.musicweather
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
 
 @SpringBootApplication
 class MusicweatherApplication
--- src/main/kotlin/com/music/weather/musicweather/music/controllers/MusicController.kt
+++ src/main/kotlin/com/music/weather/musicweather/music/controllers/MusicController.kt
@@ -3,6 +3,8 @@ package com.music.weather.musicweather.music.controllers
 import com.music.weather.musicweather.music.domain.entities.City
 import com.music.weather.musicweather.music.domain.entities.Playlist
 import com.music.weather.musicweather.music.domain.service.SearchMusicService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/music")
+@Api("Music by city API")
 class MusicController @Autowired constructor(val searchMusicService: SearchMusicService){
 
     @GetMapping("/city")
+    @ApiOperation("Search a music playlist to your city based on weather")
     fun searchMusicByCityWeather(@RequestParam cityName : String) : List<Playlist>?{
         val city = City(cityName)
         return searchMusicService.searchPlaylistByCity(city)
